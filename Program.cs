67ee70a83a0799daf957185e6207f3898b1434e8
Zameer Hussain <zameer.vighio@hotmail.com>
2024-09-09 17:42:20 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿while (true)
+{
+    Console.WriteLine("\n******************** Palindrome Start ********************\n");
+    Console.Write("Please enter word : ");
+    string word = Console.ReadLine()?.ToLower();
+
+    string revers = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        revers += word[i];
+    }
+
+    if (revers == word)
+    {
+        Console.WriteLine($"Word : {word} : is Palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"Word : {word} : is not Palindrome");
+    }
+
+    Console.WriteLine("\n******************** Palindrome End ********************\n");
+}//while
